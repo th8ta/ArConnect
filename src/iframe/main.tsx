dc872b7f41dac3b4e2fb865114d7fefbf4fa6abc
Dani Gámez Franco <gmzcodes@outlook.com>
2025-01-10 11:59:36 +0100

    Remove StrictMode.

--- src/iframe/main.tsx
+++ src/iframe/main.tsx
@@ -3,12 +3,6 @@ import { ArConnectEmbeddedAppRoot } from "./iframe";
 
 import "../../assets/popup.css";
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <ArConnectEmbeddedAppRoot />
-//   </StrictMode>
-// );
-
 createRoot(document.getElementById("root")).render(
   <ArConnectEmbeddedAppRoot />
 );
