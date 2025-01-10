369ccc17e5b38f45156549f9e529ac53cd003eb5
Dani Gámez Franco <gmzcodes@outlook.com>
2025-01-10 11:52:55 +0100

    Add DevFigmaScreen to 2 more screens.

--- src/routes/embedded/auth/add-auth-provider/auth-add-auth-provider.view.tsx
+++ src/routes/embedded/auth/add-auth-provider/auth-add-auth-provider.view.tsx
@@ -1,15 +1,27 @@
-import { useAuth } from "~utils/embedded/embedded.hooks";
-import { Link } from "~wallets/router/components/link/Link";
+import { useEmbedded } from "~utils/embedded/embedded.hooks";
+import { DevButtons } from "~components/dev/buttons/buttons.component";
+import { DevFigmaScreen } from "~components/dev/figma-screen/figma-screen.component";
+
+import screenSrc from "url:/assets-beta/figma-screens/add-auth-provider.view.png";
 
 export function AuthAddAuthProviderEmbeddedView() {
-  const { authMethod } = useAuth();
+  const { authMethod } = useEmbedded();
 
   return (
-    <di v>
-      <h3>Add {authMethod}</h3>
-      <p>...</p>
-      <button disabled>Add {authMethod}</button>
-      <Link to="/auth/add-wallet">Back</Link>
-    </di>
+    <DevFigmaScreen title={`Add ${authMethod}`} src={screenSrc}>
+      <DevButtons
+        config={[
+          {
+            label: `Add ${authMethod}`,
+            onClick: () => alert("Not implemented.")
+          },
+          {
+            label: "Back",
+            to: "/auth/add-wallet",
+            variant: "secondary"
+          }
+        ]}
+      />
+    </DevFigmaScreen>
   );
 }
--- src/routes/embedded/auth/add-device/auth-add-device.view.tsx
+++ src/routes/embedded/auth/add-device/auth-add-device.view.tsx
@@ -1,12 +1,24 @@
-import { Link } from "~wallets/router/components/link/Link";
+import { DevButtons } from "~components/dev/buttons/buttons.component";
+import { DevFigmaScreen } from "~components/dev/figma-screen/figma-screen.component";
+
+import screenSrc from "url:/assets-beta/figma-screens/add-device.view.png";
 
 export function AuthAddDeviceEmbeddedView() {
   return (
-    <div>
-      <h3>Add Device</h3>
-      <p>...</p>
-      <button disabled>Add Device</button>
-      <Link to="/auth/add-wallet">Back</Link>
-    </div>
+    <DevFigmaScreen title="Add Device" src={screenSrc}>
+      <DevButtons
+        config={[
+          {
+            label: "Add Device",
+            onClick: () => alert("Not implemented.")
+          },
+          {
+            label: "Back",
+            to: "/auth/add-wallet",
+            variant: "secondary"
+          }
+        ]}
+      />
+    </DevFigmaScreen>
   );
 }
