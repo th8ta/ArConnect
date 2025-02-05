271770bdcd90b06bf857c30a083873923c19e2c9
7i7o <mparij@communitylabs.com>
2025-02-05 23:52:22 +0100

    fix bg color for payment method button

--- src/routes/popup/purchase.tsx
+++ src/routes/popup/purchase.tsx
@@ -369,7 +369,7 @@ export function PurchaseView() {
           )}
           <Line />
           <InputButton
-            style={{ background: "#242426" }}
+            style={{ background: theme.surfaceTertiary }}
             label={browser.i18n.getMessage("buy_screen_payment_method_label")}
             onClick={() => setShowPaymentSelector(true)}
             disabled={!paymentMethod}
